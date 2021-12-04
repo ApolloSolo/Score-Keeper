10d6788892cf44c56e36f9c7494c9afeaab70d34
ApolloSolo <matt1teixeira@gmail.com>
2021-12-03 20:54:14 -0500

    takes score limit input and determins winner

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const player1Button = document.querySelector('.p1');
 const player2Score = document.querySelector('.player-2');
 const player2Button = document.querySelector('.p2');
 
-const reset = document.querySelector('.reset')
+const scoreLimit = document.querySelector('.rounds');
+const reset = document.querySelector('.reset');
 
 //Increase Player 1 Score
 player1Button.addEventListener('click', () => {
     let currentScore = getScoreValue(player1Score);
     currentScore += 1;
     player1Score.innerText = currentScore;
+    isWinner(currentScore, 1);
 })
 
 //Increase Player 2 Score
@@ -18,6 +20,7 @@ player2Button.addEventListener('click', () => {
     let currentScore = getScoreValue(player2Score);
     currentScore += 1;
     player2Score.innerText = currentScore;
+    isWinner(currentScore, 2);
 })
 
 //convert <span> innerText to number value
@@ -26,7 +29,25 @@ function getScoreValue(score) {
     return p1value;
 }
 
+//convert input to number value
+function getScoreLimit() {
+    let num = parseInt(scoreLimit.value)
+    return num;
+}
+
 reset.addEventListener('click', () => {
+    resetScore();
+})
+
+function resetScore() {
     player1Score.innerText = '0';
     player2Score.innerText = '0';
-})
+}
+
+//Ran once player score === score limit
+function isWinner(score, player){
+    if(score === getScoreLimit()){
+        setTimeout(() => {  alert(`Player ${player} wins`); }, 170);
+        setTimeout(() => {  resetScore(); }, 2000);
+    }
+}
